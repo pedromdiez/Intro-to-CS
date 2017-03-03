69cf2d2471da388f57d51847faffea9fea993b3f
pedromdiez <pdieztella@gmail.com>
2017-03-03 13:17:29 +0100

    Añado ábaco

--- Udacity Intro to CS/Days_Old/__init__.py
+++ Udacity Intro to CS/Days_Old/__init__.py
@@ -6,62 +6,71 @@
 # Assume that the birthday and current date are correct dates (and no 
 # time travel). 
 #
-
-daysofmonths = [ 0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-def leap_year(year):
-    leap_day = 366
-    common_day = 365
-    if year % 4 != 0:
-        return common_day
-    elif year % 100 != 0:
-        return leap_day
-    elif year % 400 !=0:
-        return common_day
+def isLeapYear(year):
+    # From Wikipedia http://en.wikipedia.org/wiki/Leap_year
+    """
+    if (year is not exactly divisible by 4) then (it is a common year)
+    else
+    if (year is not exactly divisible by 100) then (it is a leap year)
+    else
+    if (year is not exactly divisible by 400) then (it is a common year)
+    else (it is a leap year)    
+    """
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True 
     else:
-        return leap_day
-    
-def daysBetweenDates(year1, month1, day1, year2, month2, day2):
-    #code for same year
-    if year1 == year2:
-        if month1 == month2:
-            return day2 - day1
-        days = daysofmonths[month1] - day1 
-        month1 = month1 + 1
-        while month1 < month2: 
-            if leap_year(year1) == 366:
-                daysofmonths[2] = 29 
-            days = days + daysofmonths[month1] 
-            month1 = month1 + 1
-        return days + day2
-  ################################################
-    days = daysofmonths[month1] - day1
-    month1 = month1 + 1
-    while month1 <= 12:
-        if leap_year(year1) == 366:
-            daysofmonths[2] = 29
-        days = days  + daysofmonths[month1] 
-        month1 = month1 + 1
-    #print days 
-    year1 = year1 + 1 
-    ###########################################################
-    days = days + day2 
-    month2 = month2 - 1 
-    while month2 >= 1:
-        if leap_year(year2) == 366:
-            daysofmonths[2] = 29
-        days = days + daysofmonths[month2] 
-        month2 = month2 - 1
-    #print days
-    year2 = year2 - 1 
-    ###########################################################
-    while year1 <= year2:
-        days = days + leap_year(year1)
-        year1 = year1 + 1
-    return days 
+        return False    
 
-# print
-# Test routine
+def daysInMonth(year, month):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
+
+def nextDay(year, month, day):
+    # Use correct number of days per month
+    if day < daysInMonth(year, month):
+        return year, month, day + 1
+    else:
+        if month < 12:
+            return year, month + 1, 1           
+        else:
+            return year + 1, 1, 1
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    # Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False.
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """
+    Returns the number of days between year1/month1/day1
+    and year2/month2/day2. Assumes inputs are valid dates
+    in Gregorian calendar.
+    """
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
 
 def test():
     test_cases = [((2012,1,1,2012,2,28), 58), 
@@ -69,6 +78,7 @@ def test():
                   ((2011,6,30,2012,6,30), 366),
                   ((2011,1,1,2012,8,8), 585 ),
                   ((1900,1,1,1999,12,31), 36523)]
+
     for (args, answer) in test_cases:
         result = daysBetweenDates(*args)
         if result != answer:
