860bcc6585c1ab6956901bbb81486d16cca09571
pedromdiez <pdieztella@gmail.com>
2017-03-25 09:29:38 +0100

    25-marzo

--- Udacity Intro to CS/Days_Between_Dates.py
+++ Udacity Intro to CS/Days_Between_Dates.py
@@ -6,10 +6,32 @@
 # (hence a year is 360 days, instead of 365).
 # 
 from __builtin__ import True
+# Comprobamos si el year es bisiesto
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
 
+# Hacemos que los dias coincidan con los reales de cada mes
+def daysInMonth(year, month):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
+   
 def nextDay(year, month, day):
     """Simple version: assume every month has 30 days"""
-    if day < 30:
+    if day < daysInMonth(year, month):
         return year, month, day + 1
     else:
         if month == 12:
@@ -42,9 +64,11 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     return days 
 
 def test():
-    test_cases = [((2012,9,30,2012,10,30),30), 
-                  ((2012,1,1,2013,1,1),360),
-                  ((2012,9,1,2012,9,4),3)]
+    test_cases = [((2012,1,1,2012,2,28), 58), 
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
     
     for (args, answer) in test_cases:
         result = daysBetweenDates(*args)
@@ -54,4 +78,4 @@ def test():
             print "Test case passed!"
 
 test()
-    
+  
